a0bbd78447e3b23ae96a36dbdb0b415131ca1a99
o-fl0w <odashfl0w@proton.me>
2022-09-13 16:06:01 +0200

    refactored header handler

--- internal/api/heresphere/router.go
+++ internal/api/heresphere/router.go
@@ -3,6 +3,7 @@ package heresphere
 import (
 	"github.com/Khan/genqlient/graphql"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/zerolog/log"
 	"net/http"
 )
@@ -11,7 +12,7 @@ func Router(client graphql.Client) http.Handler {
 	httpHandler := HttpHandler{Client: client}
 	r := chi.NewRouter()
 	r.Use(logContext)
-	r.Use(addHSHeader)
+	r.Use(middleware.SetHeader("HereSphere-JSON-Version", "1"))
 	r.Post("/", httpHandler.Index)
 	r.Post("/{videoId}", httpHandler.VideoData)
 	return r
@@ -21,12 +22,5 @@ func logContext(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := log.With().Str("module", "deovr").Logger().WithContext(r.Context())
 		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-func addHSHeader(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("HereSphere-JSON-Version", "1")
-		next.ServeHTTP(w, r)
 	})
 }
