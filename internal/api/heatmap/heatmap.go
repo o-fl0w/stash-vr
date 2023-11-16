c704758b6d6c4b85ac590ba39da962794ee4a79c
o-fl0w <odashfl0w@proton.me>
2023-11-16 11:01:25 +0100

    show default cover if no heatmap image is found

--- internal/api/heatmap/heatmap.go
+++ internal/api/heatmap/heatmap.go
@@ -3,7 +3,6 @@ package heatmap
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/image/draw"
 	"golang.org/x/sync/errgroup"
@@ -16,6 +15,8 @@ import (
 )
 
 var errImageNotFound = errors.New("image not found")
+var errScreenshotImageNotFound = errors.New("screenshot image not found")
+var errHeatmapImageNotFound = errors.New("heatmap image not found")
 
 func fetchImage(ctx context.Context, fileUrl string) (image.Image, error) {
 	log.Ctx(ctx).Trace().Str("url", fileUrl).Msg("Fetching image")
@@ -30,6 +31,7 @@ func fetchImage(ctx context.Context, fileUrl string) (image.Image, error) {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusNotFound {
+		log.Ctx(ctx).Debug().Msg("Image not found")
 		return nil, errImageNotFound
 	}
 
@@ -46,12 +48,13 @@ func buildHeatmapCover(ctx context.Context, coverUrl string, heatmapUrl string) 
 	chCover := make(chan draw.Image, 1)
 	chHeatmap := make(chan image.Image, 1)
 
-	g, gCtx := errgroup.WithContext(ctx)
+	g, _ := errgroup.WithContext(ctx)
 
 	g.Go(func() error {
-		cover, err := fetchImage(log.Ctx(gCtx).With().Str("image", "cover").Logger().WithContext(gCtx), coverUrl)
+		defer close(chCover)
+		cover, err := fetchImage(log.Ctx(ctx).With().Str("image", "cover").Logger().WithContext(ctx), coverUrl)
 		if err != nil {
-			return fmt.Errorf("fetch cover: %w", err)
+			return errors.Join(errScreenshotImageNotFound, err)
 		}
 		dest, ok := cover.(draw.Image)
 		if !ok {
@@ -59,27 +62,30 @@ func buildHeatmapCover(ctx context.Context, coverUrl string, heatmapUrl string) 
 			draw.Copy(dest, image.Pt(0, 0), cover, cover.Bounds(), draw.Src, nil)
 		}
 		chCover <- dest
-		close(chCover)
 		return nil
 	})
 
 	g.Go(func() error {
-		heatmap, err := fetchImage(log.Ctx(gCtx).With().Str("image", "heatmap").Logger().WithContext(gCtx), heatmapUrl)
+		defer close(chHeatmap)
+		heatmap, err := fetchImage(log.Ctx(ctx).With().Str("image", "heatmap").Logger().WithContext(ctx), heatmapUrl)
 		if err != nil {
-			return fmt.Errorf("fetch heatmap: %w", err)
+			return errors.Join(errHeatmapImageNotFound, err)
 		}
 		chHeatmap <- heatmap
-		close(chHeatmap)
 		return nil
 	})
 
-	err := g.Wait()
-	if err != nil {
-		return nil, err
-	}
-
 	cover := <-chCover
 	heatmap := <-chHeatmap
+
+	err := g.Wait()
+	if err != nil {
+		if errors.Is(err, errScreenshotImageNotFound) {
+			return nil, err
+		} else {
+			return cover, nil
+		}
+	}
 
 	heatmapCover := overlay(cover, heatmap)
 	return heatmapCover, nil
