807661c92f06ebda526fa0635edb5a373b9fd93e
o-fl0w <odashfl0w@proton.me>
2025-05-15 17:25:59 +0200

    skip verification of insecure certificates

--- internal/stash/client.go
+++ internal/stash/client.go
@@ -1,23 +1,40 @@
 package stash
 
 import (
+	"crypto/tls"
 	"github.com/Khan/genqlient/graphql"
 	"net/http"
 )
 
 type authTransport struct {
-	key string
+	apiKey string
+	rt     http.RoundTripper
 }
 
-func (t authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Add("ApiKey", t.key)
-	return http.DefaultTransport.RoundTrip(req)
+func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req2 := req.Clone(req.Context())
+	req2.Header.Add("ApiKey", t.apiKey)
+	return t.rt.RoundTrip(req2)
 }
 
 func NewClient(graphqlUrl string, apiKey string) graphql.Client {
-	htc := http.Client{}
-	if apiKey != "" {
-		htc.Transport = authTransport{key: apiKey}
+	defaultTr, _ := http.DefaultTransport.(*http.Transport)
+	transport := defaultTr.Clone()
+	transport.TLSClientConfig = &tls.Config{
+		InsecureSkipVerify: true,
 	}
-	return graphql.NewClient(graphqlUrl, &htc)
+
+	var rt http.RoundTripper = transport
+	if apiKey != "" {
+		rt = &authTransport{
+			apiKey: apiKey,
+			rt:     transport,
+		}
+	}
+
+	htc := &http.Client{
+		Transport: rt,
+	}
+
+	return graphql.NewClient(graphqlUrl, htc)
 }
