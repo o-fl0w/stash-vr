d3e6a523ae0ed93be028f2fc676b6e306c9352fa
o-fl0w <odashfl0w@proton.me>
2025-06-06 02:01:52 +0200

    minor technical tweaks

--- internal/api/heresphere/scan.go
+++ internal/api/heresphere/scan.go
@@ -28,31 +28,34 @@ type scanDataDto struct {
 func buildScan(ctx context.Context, vds map[string]*library.VideoData, baseUrl string) (*scanDocDto, error) {
 	scanDoc := scanDocDto{ScanData: make([]scanDataDto, 0, len(vds))}
 	for _, vd := range vds {
-		id := vd.Id()
-		scanData := scanDataDto{
-			id:        id,
-			Link:      getVideoDataUrl(baseUrl, id),
-			Title:     vd.Title(),
-			DateAdded: vd.SceneParts.Created_at.Format(time.DateOnly),
-			Duration:  vd.SceneParts.Files[0].Duration,
-			Tags:      getTags(vd),
-		}
-		if vd.SceneParts.Date != nil {
-			scanData.DateReleased = vd.SceneParts.Date
-		}
-		if vd.SceneParts.Rating100 != nil {
-			scanData.Rating = util.Ptr(float32(*vd.SceneParts.Rating100) / 20)
-		}
-		if vd.SceneParts.O_counter != nil {
-			scanData.Favorites = vd.SceneParts.O_counter
-		}
-		if isFavorite(vd) {
-			scanData.IsFavorite = util.Ptr(true)
-		}
-
+		scanData := videoDataToScanDocDto(vd, baseUrl)
 		scanDoc.ScanData = append(scanDoc.ScanData, scanData)
 	}
-
 	log.Ctx(ctx).Trace().Int("scenes", len(scanDoc.ScanData)).Msg("/scan")
 	return &scanDoc, nil
+}
+
+func videoDataToScanDocDto(vd *library.VideoData, baseUrl string) scanDataDto {
+	id := vd.Id()
+	scanData := scanDataDto{
+		id:        id,
+		Link:      getVideoDataUrl(baseUrl, id),
+		Title:     vd.Title(),
+		DateAdded: vd.SceneParts.Created_at.Format(time.DateOnly),
+		Duration:  vd.SceneParts.Files[0].Duration,
+		Tags:      getTags(vd),
+	}
+	if vd.SceneParts.Date != nil {
+		scanData.DateReleased = vd.SceneParts.Date
+	}
+	if vd.SceneParts.Rating100 != nil {
+		scanData.Rating = util.Ptr(float32(*vd.SceneParts.Rating100) / 20.0)
+	}
+	if vd.SceneParts.O_counter != nil {
+		scanData.Favorites = vd.SceneParts.O_counter
+	}
+	if isFavorite(vd) {
+		scanData.IsFavorite = util.Ptr(true)
+	}
+	return scanData
 }
